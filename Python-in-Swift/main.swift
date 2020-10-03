5b7db02e7f0a82a486d23224c73d4ae64fc4fae4
edenvvv <edenvvv@gmail.com>
2020-10-04 01:08:16 +0300

    Basics

--- Python-in-Swift/main.swift
+++ Python-in-Swift/main.swift
@@ -3,7 +3,7 @@ import PythonKit
 
 // print(Python.version)
 
-print("Basics")
+print("Basics:")
 
 // Convert standard Swift types to Python (as PythonObject)
 let pythonInt: PythonObject = 1
@@ -19,3 +19,40 @@ print("pythonString is: ",pythonString[0..<6])
 print("pythonRange is: ",pythonRange)
 print("pythonArray[2] is: ",pythonArray[2])
 print("pythonDict['bar'] is: ",pythonDict["bar"])
+
+
+print("")
+// Convert Python objects back to Swift.
+let int = Int(pythonInt)!
+let float = Float(pythonFloat)!
+let string = String(pythonString)!
+let range = Range<Int>(pythonRange)!
+let array1: [Int] = Array(pythonArray)!
+let dict: [String: [Int]] = Dictionary(pythonDict)!
+
+// Perform standard operations.
+// outputs are the same as Python!
+print(Float(int) + float)
+print(string.prefix(6))
+print(range)
+print(array1[2])
+print(dict["bar"]!)
+
+print("")
+
+let one: PythonObject = 1
+print(one == one)
+print(one < one)
+print(one + one)
+
+let array2: PythonObject = [1, 2, 3]
+for (i, x) in array2.enumerated() {
+  print(i, x)
+}
+
+let pythonTuple = Python.tuple([1, 2, 3])
+print("pythonTuple:", pythonTuple, "in len:", Python.len(pythonTuple))
+
+// Convert to Swift.
+let tuple = pythonTuple.tuple3
+print("swift tuple:",tuple)

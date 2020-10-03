0cb156275caebc2df02c48eea68744f630edb3e4
edenvvv <edenvvv@gmail.com>
2020-10-04 00:54:58 +0300

    first commit

--- Python-in-Swift/main.swift
+++ Python-in-Swift/main.swift
@@ -1,11 +1,21 @@
-//
-//  main.swift
-//  Python-in-Swift
-//
-//  Created by User on 04/10/2020.
-//
-
 import Foundation
+import PythonKit
 
-print("Hello, World!")
+// print(Python.version)
 
+print("Basics")
+
+// Convert standard Swift types to Python (as PythonObject)
+let pythonInt: PythonObject = 1
+let pythonFloat: PythonObject = 3.0
+let pythonString: PythonObject = "Hello Python!"
+let pythonRange: PythonObject = PythonObject(5..<10)
+let pythonArray: PythonObject = [1, 2, 3, 4]
+let pythonDict: PythonObject = ["foo": [0], "bar": [1, 2, 3]]
+
+// Perform standard operations on Python objects.
+print("pythonInt + pythonFloat = ",pythonInt + pythonFloat)
+print("pythonString is: ",pythonString[0..<6])
+print("pythonRange is: ",pythonRange)
+print("pythonArray[2] is: ",pythonArray[2])
+print("pythonDict['bar'] is: ",pythonDict["bar"])
